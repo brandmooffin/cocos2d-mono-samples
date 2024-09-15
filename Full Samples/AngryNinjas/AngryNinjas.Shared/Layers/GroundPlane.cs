01fb62066ccd15180e9b470763f67efc379a71da
Brandon Roberts <brandmooffin@outlook.com>
2024-09-15 16:41:23 -0400

    port angry ninjas and audio samples from cocossharp

---
+++ Full Samples/AngryNinjas/AngryNinjas.Shared/Layers/GroundPlane.cs
@@ -0,0 +1,71 @@
+﻿using AngryNinjas.Shared.Entities;
+using Box2D.Collision.Shapes;
+using Box2D.Common;
+using Box2D.Dynamics;
+using Cocos2D;
+using AngryNinjas.Shared.Custom;
+using AngryNinjas.Shared.Levels;
+
+namespace AngryNinjas.Shared.Layers
+{
+    public class GroundPlane : BodyNode
+    {
+        b2World theWorld;
+        string spriteImageName;
+        CCPoint initialLocation;
+
+        public GroundPlane(b2World world, CCPoint location, string spriteFileName)
+        {
+            InitWithWorld(world, location, spriteFileName);
+        }
+
+        private void InitWithWorld(b2World world, CCPoint location, string spriteFileName)
+        {
+            this.theWorld = world;
+            this.initialLocation = location;
+            this.spriteImageName = spriteFileName;
+            CreateGround();
+        }
+
+        private void CreateGround()
+        {
+            // Define the dynamic body.
+            var bodyDef = new b2BodyDef();
+            bodyDef.type = b2BodyType.b2_staticBody; //or you could use b2_staticBody
+            bodyDef.position.Set(initialLocation.X / Constants.PTM_RATIO, initialLocation.Y / Constants.PTM_RATIO);
+            b2PolygonShape shape = new b2PolygonShape();
+
+            int num = 4;
+            b2Vec2[] vertices = {
+                new b2Vec2(-1220.0f / Constants.PTM_RATIO, 54.0f / Constants.PTM_RATIO),
+                new b2Vec2(-1220.0f / Constants.PTM_RATIO, -52.0f / Constants.PTM_RATIO),
+                new b2Vec2(1019.0f / Constants.PTM_RATIO, -52.0f / Constants.PTM_RATIO),
+                new b2Vec2(1019.0f / Constants.PTM_RATIO, 54.0f / Constants.PTM_RATIO)
+            };
+
+            shape.Set(vertices, num);
+
+            // Define the dynamic body fixture.
+            var fixtureDef = new b2FixtureDef();
+            fixtureDef.shape = shape;
+            fixtureDef.density = 1.0f;
+            fixtureDef.friction = 1.0f;
+            fixtureDef.restitution = 0.1f;
+
+            CreateBodyWithSpriteAndFixture(theWorld, bodyDef, fixtureDef, spriteImageName);
+
+            if (!TheLevel.SharedLevel.IS_RETINA)
+            {
+                //non retina adjustment
+                sprite.ScaleX = 1.05f;
+
+            }
+            else
+            {
+                // retina adjustment
+                sprite.ScaleX = 2.05f;
+                sprite.ScaleY = 2.0f;
+            }
+        }
+    }
+}
---
+++ Full Samples/AngryNinjas/AngryNinjas.Shared/Levels/IntroLayer.cs
@@ -0,0 +1,50 @@
+﻿using Cocos2D;
+
+namespace AngryNinjas.Shared.Levels
+{
+    public class IntroLayer : CCLayer
+    {
+        CCSize _screenSize;
+
+        protected override void AddedToScene()
+        {
+            base.AddedToScene();
+            //get screen size
+            _screenSize = CCDirector.SharedDirector.WinSize; //CCDirector::sharedDirector()->getWinSize();
+            var background = new CCSprite("IntroLayer");
+            background.Position = new CCPoint(_screenSize.Width / 2, _screenSize.Height / 2);
+
+            // add the background as a child to this Layer
+            AddChild(background);
+
+            // Wait a little and then transition to the new scene
+            ScheduleOnce(MakeTransition, 2);
+        }
+
+        public void MakeTransition(float dt)
+        {
+            CCLog.Log("Make Transition to Level");
+            // CCDirector.SharedDirector.ReplaceScene(new CCTransitionFade(1, TheLevel.Scene, CCColor3B.White));
+            CCDirector.SharedDirector.ReplaceScene(TheLevel.GetScene());
+        }
+
+        public static CCScene Scene
+        {
+            get
+            {
+                // 'scene' is an autorelease object.
+                var scene = new CCScene();
+
+                // 'layer' is an autorelease object.
+                var layer = new IntroLayer();
+
+                // add layer as a child to scene
+                scene.AddChild(layer);
+
+                // return the scene
+                return scene;
+            }
+
+        }
+    }
+}
